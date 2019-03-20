6ec2ff5558c0b4b12920a3258fc61db60ae9ad23
florinrinja <florinrinja@outlook.com>
2019-03-20 15:27:20 +0100

    wilders

---
+++ index.js
@@ -0,0 +1,10 @@
+const Chalk = require('chalk');
+
+function hello(){
+    console.log(Chalk.red('Juliette'));
+    console.log(Chalk.blue('Romain'));
+    console.log(Chalk.green('Alexandre'));
+    console.log(Chalk.yellow('Tanguy'));
+}
+
+hello();
